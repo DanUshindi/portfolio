15300f33d36c7ceb95c66ab5a9a300f8666e5965
DanUshindi <danushindi5@gmail.com>
2022-02-10 10:53:20 +0200

    Edit linters error

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector(".header");
-const navigation = document.querySelector('.nav-bar')
+const navigation = document.querySelector('.nav-bar');
 
-hamburger.addEventListener("click",() => {
+hamburger.addEventListener("click",() =>{
   document.body.classList.toggle("nav-open");
 })
 
-navigation.addEventListener("mouseup",() => {
+navigation.addEventListener("mouseup",() =>{
   document.body.classList.remove("nav-open");
 });
