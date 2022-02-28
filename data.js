5517e2205d53fecee0c6a64354d9a42d9d884048
DanUshindi <danushindi5@gmail.com>
2022-02-28 17:24:37 +0200

    Add the hamburger button and write the js code

--- data.js
+++ data.js
@@ -1,11 +1,8 @@
-const hamburger = document.querySelector(".hamburger");
-const header = document.querySelector(".header");
-const navigation = document.querySelector('.nav-bar');
-
-hamburger.addEventListener("click",() =>{
-  document.body.classList.toggle("nav-open");
-})
-
-navigation.addEventListener("mouseup",() =>{
-  document.body.classList.remove("nav-open");
-});
+function menuFunction() {
+  var links = document.getElementById("mobile-menu-container");
+  if(links.style.display === "block"){
+      links.style.display ="none";
+  }else{
+      links.style.display ="block";
+  }
+}
