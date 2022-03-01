a381a78f171cc45839164a923fec701bda4a6f11
DanUshindi <danushindi5@gmail.com>
2022-03-01 15:46:00 +0200

    Add required changes

--- data.js
+++ data.js
@@ -8,11 +8,3 @@ function menuFunction() {
 }
 
 document.querySelector('.active-close').addEventListener('click', menuFunction);
-
-if (links.style.display === 'block') {
-  document.querySelector('.ellipse-three').style.display = 'block';
-  document.querySelector('.ellipse-four').style.display = 'block';
-} else {
-  document.querySelector('.ellipse-three').style.display = 'none';
-  document.querySelector('.ellipse-four').style.display = 'none';
-}
