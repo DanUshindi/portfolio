59ac3b77fa28488cce5f07752848ae704b4cfde1
DanUshindi <danushindi5@gmail.com>
2022-03-01 16:23:49 +0200

    Fix the problem with the overflow

--- data.js
+++ data.js
@@ -1,10 +1,14 @@
 const links = document.getElementById('mobile-menu-container');
+const html = document.querySelector('html');
+const closeMenu = document.querySelector('.active-close');
+
 function menuFunction() {
   if (links.style.display === 'block') {
     links.style.display = 'none';
   } else {
     links.style.display = 'block';
+    html.style.overflow = 'hidden';
   }
 }
 
-document.querySelector('.active-close').addEventListener('click', menuFunction);
+closeMenu.addEventListener('click', menuFunction);
