639d82b0d6abfdae9279f926aa56328589620394
DanUshindi <danushindi5@gmail.com>
2022-03-01 13:53:07 +0200

    Add required changes

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
+const links = document.getElementById('mobile-menu-container');
 function menuFunction() {
-  const links = document.getElementById('mobile-menu-container');
   if (links.style.display === 'block') {
     links.style.display = 'none';
   } else {
@@ -8,3 +8,11 @@ function menuFunction() {
 }
 
 document.querySelector('.active-close').addEventListener('click', menuFunction);
+
+if (links.style.display === 'block') {
+  document.querySelector('.ellipse-three').style.display = 'block';
+  document.querySelector('.ellipse-four').style.display = 'block';
+} else {
+  document.querySelector('.ellipse-three').style.display = 'none';
+  document.querySelector('.ellipse-four').style.display = 'none';
+}
