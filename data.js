df555e607abc41cc16cf641cace00a6a84680577
DanUshindi <danushindi5@gmail.com>
2022-03-02 12:05:40 +0200

    Fix the mobile popup

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
 const links = document.getElementById('mobile-menu-container');
 const html = document.querySelector('html');
 const closeMenu = document.querySelector('.active-close');
+const closePop = document.querySelector('.vide');
+
+const bigContainner = document.querySelector('.big-container-doc');
 
 function menuFunction() {
   if (links.style.display === 'block') {
@@ -12,3 +15,18 @@ function menuFunction() {
 }
 
 closeMenu.addEventListener('click', menuFunction);
+
+function togglepopup() {
+  document.querySelector('.popup').classList.toggle('active');
+
+  bigContainner.classList.toggle('active');
+
+  closePop.classList.remove('active');
+
+  if (bigContainner.style.display === 'none') {
+    html.style.overflow = 'hidden';
+  } else {
+    html.style.overflow = 'scroll';
+  }
+}
+closePop.addEventListener('click', togglepopup);
